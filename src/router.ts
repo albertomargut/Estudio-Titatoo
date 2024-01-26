3239f0a25626f2760d923a4af8a18b201d0d9336
albertomargut <alberto.martinguti@gmail.com>
2024-01-26 23:08:03 +0100

    router updated with appointments

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import appointmentRoutes from "./routes/appointments.routes";
 
 // -----------------------------------------------------------------------------
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.use("/api/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/appointments/", appointmentRoutes);
 
 
 export default router;
