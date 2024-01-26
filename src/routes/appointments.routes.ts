fc8dbc14e1027ba393bd6a0126856a47689941df
albertomargut <alberto.martinguti@gmail.com>
2024-01-26 22:26:05 +0100

    appointment.routes created

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { AppointmentController } from "../controllers/AppointmentController";
+
+// ----
+const router = express.Router();
+const appointmentController = new AppointmentController();
+
+router.get("/", appointmentController.getAll)
+router.post("/", appointmentController.create)
+router.patch("/:id", appointmentController.update);
+router.get("/users/:id", appointmentController.getById)
+router.get("/artists/:id", appointmentController.getByArtist)
+
+export default router;
+
